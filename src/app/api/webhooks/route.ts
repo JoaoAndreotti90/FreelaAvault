108a2d20a765736ea92e8d103f6ab927b3a40fe1
João Andreotti <joaogilberto238@gmail.com>
2025-12-31 09:27:25 -0300

    Fix Stripe API version

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
   const signature = headers().get("Stripe-Signature") as string
 
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2023-10-16",
+    apiVersion: "2025-12-15.clover", 
     typescript: true,
   })
 
